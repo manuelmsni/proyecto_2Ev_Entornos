3b5227f14094241f11b3d8fb6d43f9c101a92a65
manuelmsni <manuelmsni@DESKTOP-SUL1L5P>
2023-02-25 20:13:12 +0100

    Agregando la clase "ManuelMartinSantamariaIdentificacionTaxonomica" al Staging y haciendo el primer commit.

---
+++ src/proyectoManuelMartinSantamaria/ManuelMartinSantamariaIdentificacionTaxonomica.java
@@ -0,0 +1,10 @@
+package proyectoManuelMartinSantamaria;
+
+public class ManuelMartinSantamariaIdentificacionTaxonomica {
+	private String familia, genero, hobbie;
+	public ManuelMartinSantamariaIdentificacionTaxonomica(String familia, String genero, String hobbie) {
+		this.familia = familia;
+		this.genero = genero;
+		this.hobbie = hobbie;
+	}
+}
